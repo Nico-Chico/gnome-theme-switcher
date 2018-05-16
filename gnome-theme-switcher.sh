34b16cb478cdaeeda7cc02883a32f1b12fde3b44
Nico-chico <38226481+Nico-chico@users.noreply.github.com>
2018-05-16 20:26:01 +0200

    Update gnome-theme-switcher.sh

--- gnome-theme-switcher.sh
+++ gnome-theme-switcher.sh
@@ -2,8 +2,8 @@
 # gnome-theme-switcher	v0.1		 by Nico Magro Cruzado (Nico_chico)
 
 # CONFIG:
-theme1="Adwaita"		#Choose your first gtk theme
-theme2="Adwaita-dark"	#Choose your second gtk theme
+theme1="Adwaita"      #Choose your first gtk theme
+theme2="Adwaita-dark" #Choose your second gtk theme
 # ----------
 
 if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" = "'$theme1'" ]; then
